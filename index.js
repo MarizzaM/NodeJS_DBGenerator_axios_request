f9f18a53fe23ffe065e7935ea0727ec3f68680ed
Marisha <75089445+MarizzaM@users.noreply.github.com>
2021-08-17 17:20:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const axios = require('axios');
 
 const makeGetRequest = async () => {
+  
   try {
-    const response = await axios.get("https://randomuser.me/api", {
-
-    });
+    
+    const response = await axios.get("https://randomuser.me/api");
     console.log(`\nName: ${response.data.results[0].name.first}`);
     console.log(`City: ${response.data.results[0].location.country}`);
+
   } catch (err) {
+
     console.log("received error: ", err.toJSON());
   }
 };
 
-for(let i = 0; i < 10; i++){
+var i = 0, howManyTimes = 10;
+
+function f() {
   makeGetRequest();
+  i++;
+  if (i < howManyTimes) {
+    setTimeout(f, 2000);
+  }
 }
 
+f();
